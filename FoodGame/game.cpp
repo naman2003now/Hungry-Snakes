06cf5fab9de3201fb5f35c84da4a0a2fbe3bcc3a
naman2003now <69296186+naman2003now@users.noreply.github.com>
2021-10-16 21:48:35 +0530

    Update game.cpp

--- FoodGame/game.cpp
+++ FoodGame/game.cpp
@@ -1,14 +1,16 @@
 #include <SFML\Graphics.hpp>
-#include <iostream>
 #include "Snake.h"
 #include "Food.h"
+#include<Windows.h>
+
 
 #define BGCOLOR sf::Color(50, 50, 50)
 #define SNAKECOLOR sf::Color(250, 250, 250)
 #define FOODCOLOR sf::Color(250, 50, 50)
 
 int main() {
-
+	HWND hWnd = GetConsoleWindow();
+	ShowWindow(hWnd, SW_HIDE);
 	int WINDOW_HEIGHT = 800;
 	int WINDOW_WIDTH = 800;
 
@@ -21,12 +23,17 @@ int main() {
 	char direction = 'r';
 	
 	sf::Clock frameTime;
-	float frameRate = 10.0f;
+	float frameRate = 15.0f;
+
+	sf::Clock snakeTime;
+	float snakeRate = 10.0f;
+
+	sf::Clock snakeSpwanner;
+	float snakeSpawnTime = 10.0f;
 
 	Food food(cellSize, FOODCOLOR);
 	std::vector<Snake> allSnakes;
-	allSnakes.push_back(Snake(cellSize, SNAKECOLOR));
-	allSnakes.push_back(Snake(cellSize, SNAKECOLOR));
+	allSnakes.push_back(Snake(cellSize, gridSize , SNAKECOLOR, 10));
 
 	while (window.isOpen()) {
 		while (window.pollEvent(event)) {
@@ -49,14 +56,37 @@ int main() {
 		//Update Every gameframe
 		if (frameTime.getElapsedTime().asSeconds() > 1.0f / frameRate) {
 			food.update(direction);
+			bool gameOver = false;
 			for (int i = 0; i < allSnakes.size(); i++) {
-				allSnakes[i].update(allSnakes);
+				for (int j = 0; j < allSnakes[i].body.size(); j++) {
+					if (allSnakes[i].body[j] == food.position) {
+						allSnakes.clear();
+						allSnakes.push_back(Snake(cellSize, gridSize, SNAKECOLOR, 10));
+						gameOver = true;
+						break;
+					}
+				}
+				if (gameOver) break;
 			}
+
 			frameTime.restart();
+		}
+		if (snakeTime.getElapsedTime().asSeconds() > 1.0f / snakeRate) {
+			for (int i = 0; i < allSnakes.size(); i++) {
+				allSnakes[i].update(allSnakes, food.position);
+			}
+			snakeTime.restart();
+		}
+		if (snakeSpwanner.getElapsedTime().asSeconds() > snakeSpawnTime) {
+			allSnakes.push_back(Snake(cellSize, gridSize, SNAKECOLOR, 10));
+			snakeSpwanner.restart();
 		}
 
 
 		window.clear(BGCOLOR);
+		for (int i = 0; i < allSnakes.size(); i++) {
+			allSnakes[i].drawBounds(window);
+		}
 		for (int i = 0; i < allSnakes.size(); i++) {
 			allSnakes[i].draw(window);
 		}
